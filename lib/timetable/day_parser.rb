ee047e070b266969313025469e0c13137021b841
Wesley Moore <wez@wezm.net>
2010-10-03 18:20:31 +1100

    Add day parser

---
+++ lib/timetable/day_parser.rb
@@ -0,0 +1,25 @@
+# vi: set fileencoding=utf-8 :
+
+module Timetable
+  module DayParser
+    
+    DAYS = %w[SUN MON TUE WED THU FRI SAT]
+    
+    def self.match(text)
+      @days_re ||= Regexp.new(DAYS.join('|'), true)
+      @days_re.match(text)
+    end
+
+    def self.parse(text)
+      from, to = text.split(/\s*[­-]\s*/) # utf-8 dash, ASCII dash
+
+      from = DAYS.index(from[0,3].upcase) + 1
+      to = DAYS.index(to[0,3].upcase) + 1 if(to)
+
+      raise RuntimeError.new("Invalid day spec: #{text}") unless from
+
+      to ? (from..to) : [from]
+    end
+
+  end
+end
---
+++ spec/day_parser_spec.rb
@@ -0,0 +1,44 @@
+# vi: set fileencoding=utf-8 :
+
+require 'spec_helper'
+require 'timetable/day_parser'
+
+describe Timetable::DayParser do
+
+  describe "match" do
+    it "should be true for MONDAY - FRIDAY" do
+      Timetable::DayParser.match("MONDAY - FRIDAY").should be_true
+      Timetable::DayParser.match("Monday ­ Friday").should be_true # Multibyte dash
+    end
+    
+    it "should be true for FRI, SAT & SUN" do
+      Timetable::DayParser.match("Fri").should be_true
+      Timetable::DayParser.match("FRI").should be_true
+      Timetable::DayParser.match("Sat").should be_true
+      Timetable::DayParser.match("Sun").should be_true
+    end
+  end
+  
+  describe "parse" do
+    
+    it "should return 2..6 for Monday ­ Friday (utf-8 dash)" do
+      Timetable::DayParser.parse("Monday ­ Friday").should == (2..6)
+    end
+    
+    it "should return 2..6 for MONDAY ­ FRIDAY (utf-8 dash)" do
+      Timetable::DayParser.parse("MONDAY ­ FRIDAY").should == (2..6)
+    end
+
+    it "should return 2..6 for Monday - Friday (ASCII dash)" do
+      Timetable::DayParser.parse("Monday - Friday").should == (2..6)
+    end
+
+    it "should return [6] for Fri" do
+      Timetable::DayParser.parse("Fri").should == [6]
+    end
+
+    it "should return [1] for Sun" do
+      Timetable::DayParser.parse("Sun").should == [1]
+    end
+  end
+end
