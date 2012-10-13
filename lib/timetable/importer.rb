893958ae4bb2270cf2ea044cf2af80bffeb1dc04
Wesley Moore <wes@wezm.net>
2012-10-13 22:34:35 +1100

    Import stops

--- lib/timetable/importer.rb
+++ lib/timetable/importer.rb
@@ -46,7 +46,7 @@ module Timetable
       services.each do |service_data|
         # Create the service
         enum = service_data.each
-        timetable.add_service(
+        service = timetable.add_service(
           :days => encode_days(enum.next),
           :number => enum.next,
           :vehicle => enum.next,
@@ -57,29 +57,59 @@ module Timetable
         )
 
         # Import the stops on the service
-        create_stops! enum
+        create_stops_on_service! service, enum
       end
     end
 
+    # The days value is a bitmask with Mon = bit 0, Tue bit 1, etc.
     def encode_days(days_string)
       days = %w[MON TUE WED THU FRI SAT SUN]
-      if days_string.include?('-')
+      if days_string.include?('-') # Day range
         start_day, end_day = days_string.upcase.split('-')
         days.index(start_day)..days.index(end_day)
-      elsif days_string.include?(',')
+      elsif days_string.include?(',') # Day set
         days_string.upcase.split(',').map do |day|
           days.index day
         end
       else
-        [days.index(days_string.upcase)]
+        [days.index(days_string.upcase)] # Single day
       end.inject(0) do |memo, day|
         memo | (1 << day)
       end
     end
 
-    def create_stops!(stops)
+    def create_stops_on_service!(service, stops)
       position = 0
 
+      while true
+        begin
+          # Find the timetable station for this position
+          timetable_station = TimetableStation.find(
+            :timetable => timetable,
+            :position  => position
+          )
+
+          stop_time = stops.next
+          if !stop_time.nil?
+            if stop_time =~ /^(..:..)([^0-9])?$/
+              time = $1
+              flag = $2
+            else
+              puts "Warning: #{stop_time} doesn't match expected time format"
+            end
+
+            service.add_stop(
+              :timetable_station => timetable_station,
+              :time => time,
+              :flag => flag
+            )
+          end
+
+          position += 1
+        rescue StopIteration
+          return
+        end
+      end
     end
 
   end
